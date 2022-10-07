cc756cb92cc2229671df63b91a9f4457d8439c29
taylorgrant <taylorgrant38@gmail.com>
2022-10-07 00:02:07 +0000

    getting viz involved

--- app.R
+++ app.R
@@ -7,12 +7,14 @@ pacman::p_load(tidyverse, janitor, here, glue, qsurvey, qualtRics, shinydashboar
 #   dplyr::mutate(creationDate = as.Date(creationDate)) %>%
 #   dplyr::arrange(desc(creationDate))
 # saveRDS(sids, "/srv/shiny-server/qualtrics-viz/data/qualtrics_sids.rds")
-
+source("theme_xf.R")
 # load Qualtrics Survey IDs
 sids <- readRDS("/srv/shiny-server/qualtrics-viz/data/qualtrics_sids.rds")
 
 # load helpers
 source('/srv/shiny-server/qualtrics-viz/helpers.R')
+source('/srv/shiny-server/qualtrics-viz/singleQ_summary.R')
+source('/srv/shiny-server/qualtrics-viz/singleQ_plot.R')
 
 # SHINY APP  --------------------------------------------------------------
 
@@ -46,7 +48,8 @@ body <- dashboardBody(
   # tags$head(tags$link(rel = "shortcut icon", href = "favicon.ico")),
   tabItems(
     # conditionally render the output using inputs (see renderUI below)
-    tabItem("gfk", uiOutput("tab1"))
+    tabItem("gfk", uiOutput("tab1"),
+            shinysky::busyIndicator(text = 'Please wait...', wait = 150))
   )
 )
 
@@ -73,10 +76,10 @@ server = function(input, output, session) {
   })
   
   # 2. Import Survey button to trigger the import from API
-  observeEvent(input$surveySelect, {   
+  observeEvent(input$surveySelect, {
     output$tab1 <- renderUI({
       tab1_ui <- tabItem("gfk")
-      if (input$surveySelect == '') return() 
+      if (input$surveySelect == '') return()
       actionButton("importSurvey", "Import Survey", icon = icon("upload"),
                    width = "200px")
     })
@@ -84,66 +87,74 @@ server = function(input, output, session) {
   
   # 3. Importing survey brings summary data to make sure proper survey loaded 
   observeEvent(input$importSurvey, {
-    # load the survey here
+    # load the survey on button push
     # load_survey(sids[sids$name == input$surveySelect,]$id)
     output$tab1 <- renderUI({
       tabItem("gfk", h4("Imported Survey"), #value = "test1",
               fluidRow(
                 box(width = 5,
                     renderTable(data.frame(Metadata = c("Name", "ID", "Created", "Responses", "Questions", "Blocks"),
-                                           Response = c(d$name, d$id, d$creationDate, d$responseCounts$auditable, 
-                                                   length(d$questions), length(d$blocks)))))
+                                           Response = c(d$name, d$id, d$creationDate, d$responseCounts$auditable,
+                                                   length(d$questions), length(d$blocks))))
+                    )
               ))
     })
     
     # 4a. On load, pull in the survey blocks
     output$blockContents <- renderUI({
       selectInput("block_contents", "Survey Block",
-                  choices = c(unique(toc$block), "RESET / ALL BLOCKS"))
+                  choices = c("", unique(toc$block), "RESET / ALL BLOCKS"))
     })
-    
-    # 4b. On load, pull in the survey questionnaire
+
+  #   # 4b. On load, pull in the survey questionnaire
     output$tableContents <- renderUI({
+      toc_list <- c("", toc$question_id)
+      names(toc_list) <- c("", paste0("Q", toc$question_order, ": ", toc$question_text))
+      # names are seen by user, id is on the backend
       selectInput("table_contents", "Question",
-                  c("", paste0("Q", toc$question_order, ": ", toc$question_text)))
+                  choices = toc_list)
     })
   })
   
   # 5. Allow the Survey Block selection to filter the questionnaire 
-  observe({
+  observeEvent(input$block_contents, {
     # call helper to filter questions as necessary
     updated_toc <- toc_filter(toc, input$block_contents)
-
+    # update
     updateSelectInput(
-      session = session, 
+      session = session,
       inputId = "table_contents",
       choices = c("", updated_toc)
     )
   })
   
-  observeEvent(input$table_contents, {   
+  # Render in the main dashboard
+  observeEvent(input$table_contents, {
     output$tab1 <- renderUI({
-      tab1_ui <- tabItem("gfk", h4("Quarterly GFK Survey Response"), value="test1",
+      tab1_ui <- tabItem("gfk", h4("Survey Results"), value="test1",
                          fluidRow(
                            box(
-                             width = 8,
-                             renderTable(toc %>% 
-                                  filter(question_text == trimws(gsub(".*\\:", "", input$table_contents))))
-                             # highchartOutput("hcontainer", height = "650px")
+                             width = 5,
+                             renderPlot(question_summary(input$table_contents)$p1)
                            ),
                            box(
                              width = 3,
                              title_side = "left",
-                             title = "About the GfK Data:"
-                             # htmlOutput("about")
+                             title = "Download",
+                             downloadButton("downloadPlot1", "Download Plot")
                            )
                          ),
                          fluidRow(
                            box(
+                             width = 5,
+                             renderPlot(question_summary(input$table_contents)$p1_flip)
+                             # renderTable(question_summary(input$table_contents)$data)
+                           ),
+                           box(
                              width = 3,
                              title_side = "left",
-                             title = "Download"
-                             # downloadButton("downloadPlot", "Download Plot")
+                             title = "Download",
+                             downloadButton("downloadPlot2", "Download Plot")
                            )
                          )
       )
@@ -151,7 +162,27 @@ server = function(input, output, session) {
     })
 })
   
+  # download handler with size and resolution set
+  output$downloadPlot1 <- downloadHandler(
+    filename = function(){
+      paste0(d$name, "-Q", toc[toc$question_id == input$table_contents,]$question_order, ".png")
+      # paste0(d$name, "-Q", toc[toc$input$table_contents,]$question_order, ".png")
+    },
+    content = function(file){
+      req(question_summary(input$table_contents)$p1)
+      ggsave(file, plot = question_summary(input$table_contents)$p1, device = 'png', width = 6.8, height = 6.3, unit = "in")
+    }
+  )
   
+  output$downloadPlot2 <- downloadHandler(
+    filename = function(){
+      paste0(d$name, "-Q", toc[toc$question_id == input$table_contents,]$question_order, "-flip.png")
+    },
+    content = function(file){
+      # req(out$p1)
+      ggsave(file, plot = question_summary(input$table_contents)$p1_flip, device = 'png', width = 6.8, height = 6.3, unit = "in")
+    }
+  )
   
   
 }
